3b9e2f7b8712f56cc9a93e54cd15b7cca7e99dc3
jiaruihua <jiaruihua@ksjgs.com>
2021-02-07 23:10:31 +0800

    增加 daliy Exercise First page

---
+++ lib/home-pages/daily-exercise/DaliyConstant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+const kdBackgroundColor = Color(0xFFF8F8F8);
+const kdActiveIconColor = Color(0xFFE68342);
+const kdTextColor = Color(0xFF222B45);
+const kdBlueLightColor = Color(0xFFC7B8F5);
+const kdBlueColor = Color(0xFF817DC0);
+const kdShadowColor = Color(0xFFE6E6E6);
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutterApp/home-pages/covid/covid.dart';
+import 'package:flutterApp/home-pages/daily-exercise/DailyExerciseHome.dart';
 import 'package:flutterApp/home-pages/iniherit-test/inherit_page.dart';
 import 'package:flutterApp/home-pages/onboading/BoadingHome.dart';
 import 'package:flutterApp/home-pages/sliver-demo/SliverHome.dart';
 
 class Routes {
   static final pageNames = [
+    RouteInfo(name: "Daily-Exercise"),
     RouteInfo(name: "Covid-19"),
     RouteInfo(name: "InheritedWidget"),
     RouteInfo(name: "OnBoarding"),
@@ -16,6 +18,7 @@ class Routes {
     "/OnBoarding": (context) => HomeBoardingApp(),
     "/InheritedWidget": (context) => InheritPage(),
     "/Covid-19": (context) => CovidHomePage(),
+    "/Daily-Exercise": (context) => DailyExerciseHome(),
   };
 }
 
